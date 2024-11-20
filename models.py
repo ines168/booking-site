c7a12ba9ad40db3f7e16302157a97e5845bb1193
Ines <ines.cvek16@gmail.com>
2024-11-20 18:53:55 +0100

    update models

--- models.py
+++ models.py
@@ -6,11 +6,14 @@ class Venue(db.Model):
     __tablename__ = 'venues'
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String, nullable=False)
+    city=db.Column(db.String, nullable=False)
+    state=db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
     phone = db.Column(db.String, nullable=False)
-    city = db.Column(db.String, nullable=False)
-    shows = db.relationship('Show', backref='venues', lazy=True, cascade='all, delete-orphan')
-    # state, link
+    image_link = db.Column(db.String, nullable=False)
+    website_link = db.Column(db.String)
+    facebook_link = db.Column(db.String)
+    shows = db.relationship('Show', backref='venue', lazy=True, cascade='all, delete')
 
     def __repr__(self):
         return f'<Venue: {self.name}>'
@@ -20,9 +23,14 @@ class Artist(db.Model):
     __tablename__ = 'artists'
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String, nullable=False)
-    address=db.Column(db.String, nullable=False)
-    phone=db.Column(db.String, nullable=False)
-    shows=db.relationship('Show', backref='artists', lazy=True, cascade='all, delete-orphan')
+    city=db.Column(db.String, nullable=False)
+    state=db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String, nullable=False)
+    image_link = db.Column(db.String, nullable=False)
+    website_link = db.Column(db.String)
+    facebook_link = db.Column(db.String)
+    shows=db.relationship('Show', backref='artist', lazy=True, cascade='all, delete')
 
     def __repr__(self):
         return f'<Artist: {self.name}>'
@@ -31,6 +39,6 @@ class Show(db.Model):
     __tablename__ = 'shows'
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String, nullable=True)
-    date=db.Column(db.String, nullable=True)
+    start_time=db.Column(db.DateTime)
     venue_id=db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
     artist_id=db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
